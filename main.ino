439dd3da8790e59d1520fd970b0bf133aa0ae885
osbarros <ceobarros@outlook.com>
2022-08-04 14:37:28 -0300

    implementação da lógica de troca de efeito com o clique do botão

--- main.ino
+++ main.ino
@@ -5,49 +5,60 @@
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRB + NEO_KHZ800);
 
-
+int estadoBotao = 0; // inicia com o valor de 0 no qual nenhum efeito é exibido
 
 void setup(){
   
   	pixels.begin();
-  
-  
-}
-
-
-
-
-void loop () {
-  
-  
-  for(int i = 0; i < NUM_LEDS ; i++){
-  	pixels.setPixelColor(i, pixels.Color(0, 100, 0));
-    pixels.setPixelColor(i-3, pixels.Color(0, 0, 0));
-    pixels.show();
-    delay(100);
-  }
-  
-  pixels.setPixelColor(25, pixels.Color(0, 0, 0));
-  pixels.setPixelColor(26, pixels.Color(0, 0, 0));
-  pixels.setPixelColor(27, pixels.Color(0, 0, 0));
-  pixels.show();
-  delay(100);
+    pinMode(1, INPUT)
   
 }
 
+void loop () {  
+    
+    if (digitalRead == HIGH && estadoBotao != 4) {
+        estadoBotao++;
+    }
 
-void loop () {
+    else if (digitalRead == HIGH && estadoBotao == 3){
+        estadoBotao = 0;
+    }
+
+    switch(estadoBotao){
+
+        case 0: //Acende luz branca
+            for(int i = 0; i < NUM_LEDS ; i++){
+  	            pixels.setPixelColor(i, pixels.Color(0, 0, 0));
+                pixels.show();
+            }
+
+        case 1: //feixe de luz
+           for(int i = 0; i < NUM_LEDS ; i++){
+  	            pixels.setPixelColor(i, pixels.Color(0, 100, 0));
+                pixels.setPixelColor(i-3, pixels.Color(0, 0, 0));
+                pixels.show();
+                delay(100);
+            }
   
-  int red = random(0, 255);
-  int green = random(0, 255);
-  int blue = random(0,255);
-  
-  
-  for(int i = 0; i < NUM_LEDS ; i++){
-  	pixels.setPixelColor(i, pixels.Color(red, green, blue));
-    pixels.show();
-    delay(25);
-  }
-  
-  
+            pixels.setPixelColor(25, pixels.Color(0, 0, 0));
+            pixels.setPixelColor(26, pixels.Color(0, 0, 0));
+            pixels.setPixelColor(27, pixels.Color(0, 0, 0));
+            pixels.show();
+            delay(100);
+
+        case 2: // Acende luz verde
+            for(int i = 0; i < NUM_LEDS ; i++){
+  	            pixels.setPixelColor(i, pixels.Color(100, 0, 0));
+                pixels.show();
+                delay(100);
+            }
+
+        case 2: // Acende luz vernelha
+            for(int i = 0; i < NUM_LEDS ; i++){
+  	            pixels.setPixelColor(i, pixels.Color(0, 110, 0));
+                pixels.show();
+                delay(100);
+            }
+    }
 }
+  
