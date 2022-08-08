81b58aea0e14a998c81177b9c2138564c114bb77
osbarros <ceobarros@outlook.com>
2022-08-08 15:24:13 -0300

    correção de código

--- main.ino
+++ main.ino
@@ -2,6 +2,7 @@
 
 #define PIN 2
 #define NUM_LEDS 28
+#define botao 4
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRB + NEO_KHZ800);
 
@@ -10,54 +11,56 @@ int estadoBotao = 0; // inicia com o valor de 0 no qual nenhum efeito é exibido
 void setup(){
   
   	pixels.begin();
-    pinMode(1, INPUT);
+    pinMode(botao, INPUT);
+  	Serial.begin(9600);
   
 }
 
 void loop () {  
-    
-    if (digitalRead(1) == HIGH && estadoBotao != 3) 
-        estadoBotao++;
-
-    else if (digitalRead(1) == HIGH && estadoBotao == 3)
-        estadoBotao = 0;
-    
-
+   
+      
     switch(estadoBotao){
 
-        case 0: //Acende luz branca
+        case 0: //Acende luz azul
+      		//Serial.print(digitalRead(botao) == HIGH);
             for(int i = 0; i < NUM_LEDS ; i++){
-  	            pixels.setPixelColor(i, pixels.Color(0, 0, 0));
-                pixels.show();
+  	        	pixels.setPixelColor(i, pixels.Color(0, 0, 100));
+              	pixels.show();
+				delay(100);
+              	alteraEfeito();
             }
+      		break;
 
-        case 1: //feixe de luz
-           for(int i = 0; i < NUM_LEDS ; i++){
-  	            pixels.setPixelColor(i, pixels.Color(0, 100, 0));
-                pixels.setPixelColor(i-3, pixels.Color(0, 0, 0));
-                pixels.show();
-                delay(100);
-            }
-  
-            pixels.setPixelColor(25, pixels.Color(0, 0, 0));
-            pixels.setPixelColor(26, pixels.Color(0, 0, 0));
-            pixels.setPixelColor(27, pixels.Color(0, 0, 0));
-            pixels.show();
-            delay(100);
-
-        case 2: // Acende luz verde
+        case 1: // Acende luz verde
+      		//Serial.print(digitalRead(botao) == HIGH);
             for(int i = 0; i < NUM_LEDS ; i++){
   	            pixels.setPixelColor(i, pixels.Color(100, 0, 0));
                 pixels.show();
                 delay(100);
-            }
+                alteraEfeito();
+            }             
+      		break;
 
-        case 3: // Acende luz vernelha
+        case 2: // Acende luz vernelha
+  
             for(int i = 0; i < NUM_LEDS ; i++){
   	            pixels.setPixelColor(i, pixels.Color(0, 110, 0));
                 pixels.show();
                 delay(100);
+              	alteraEfeito();
             }
+      		break;
     }
+}
+
+
+void alteraEfeito() {
+    
+	if (digitalRead(botao) == HIGH && estadoBotao != 2) 
+    	estadoBotao++;
+
+   	else if (digitalRead(botao) == HIGH && estadoBotao == 2)
+       	estadoBotao = 0;
+    
 }
   
